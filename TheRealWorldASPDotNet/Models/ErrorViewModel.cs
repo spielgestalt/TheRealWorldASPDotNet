090184faaac4a2d04e06cbed7bd80053aefe58ec
Carsten Fregin <fregin@me.com>
2019-04-09 10:37:43 +0200

    initial commit

---
+++ TheRealWorldASPDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheRealWorldASPDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
